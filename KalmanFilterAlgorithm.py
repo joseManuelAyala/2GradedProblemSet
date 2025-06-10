b1300b9f51e81cfab036ae3e527bccc066c0d60e
uwtgm <uwtgm@student.kit.edu>
2025-06-10 14:10:10 +0200

    Kalman
    Glande

--- KalmanFilterAlgorithm.py
+++ KalmanFilterAlgorithm.py
@@ -2,14 +2,36 @@ import numpy as np
 
 
 
-def kalman_filter(training_set, initial_state_mean, initial_state_var):
+def kalman_filter(training_set, initial_state_mean, initial_state_var, B, H, a, c, phi, Q):
     T = len(training_set)
-    # Initialize Kalman Filter with Expert Knowledge
 
+    # Initialize Kalman Filter with Expert Knowledge
     predicted_mean = initial_state_mean
     predicted_var = initial_state_var
 
     # Output Arrays
-
     filtered_state_means = np.zeros(T)
     filtered_state_vars = np.zeros(T)
+
+    for t in range(T):
+
+        # Kalman Gain
+        k = predicted_var * B / (B * predicted_var * B + H)
+
+        # Prediction Error
+        v_t = training_set[t] - (a + B * predicted_mean)
+
+        # Update posterior for alpha_t
+        updated_mean = predicted_mean + k * v_t
+        updated_var = (1 - k * B) * predicted_var
+
+        # Save estimations in arrays
+        filtered_state_means[t] = updated_mean
+        filtered_state_vars[t] = updated_var
+
+        # Predict next state alpha_t+1
+        predicted_mean = c + phi * updated_mean
+        predicted_var = phi * updated_var * phi + Q
+
+    # Return with Kalman Filter filtered means and variances
+    return filtered_state_means, filtered_state_vars
--- SimulationSVM.py
+++ SimulationSVM.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from KalmanFilterAlgorithm import kalman_filter
 
 # Fix seed for our test-case
 np.random.seed(1234)
@@ -24,15 +25,39 @@ for t in range(1, T):
 xi = np.random.normal(0, 1, T)
 r = sigma * np.exp(0.5 * h_true) * xi
 
+# Apply Kalman Filter
+
+# Convert returns with log
+log_squared_returns = np.log(r**2 + 1e-6)
+
+# Define Parameters for the Kalman Filter
+a = 0
+B = 1
+H = 0.2
+
+c = 0
+Q = sigma_eta**2
+
+initial_state_mean = 0
+initial_state_var = 1
+
+# Call Kalman Filter
+
+filtered_means, filtered_vars = kalman_filter(log_squared_returns, initial_state_mean,
+                                              initial_state_var, B, H, a, c, phi, Q)
+
+
 # Plot volatility and returns
 plt.figure(figsize=(12, 6))
 
-# Plot latent volatility
+# Plot latent volatility and Kalman estimate
 plt.subplot(2, 1, 1)
-plt.plot(h_true, color='darkorange')
-plt.title("Latent Log-Volatility $h_t$")
+plt.plot(h_true, color='darkorange', label='True $h_t$')
+plt.plot(filtered_means, color='blue', alpha=0.7, label='Kalman $\hat{h}_t$')
+plt.title("Latent and Estimated Log-Volatility")
 plt.xlabel("Time")
 plt.ylabel("$h_t$")
+plt.legend()
 
 # Plot returns
 plt.subplot(2, 1, 2)
@@ -40,6 +65,7 @@ plt.plot(r, color='steelblue')
 plt.title("Simulated Returns $r_t$")
 plt.xlabel("Time")
 plt.ylabel("$r_t$")
+
 
 plt.tight_layout()
 plt.show()
