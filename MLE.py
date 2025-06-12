4ad524aaf619b2a626ac0f0a2c0851342b4533fb
uwtgm <uwtgm@student.kit.edu>
2025-06-12 21:01:57 +0200

    m

--- MLE.py
+++ MLE.py
@@ -4,8 +4,8 @@ from KalmanFilterAlgorithm import kalman_filter
 # Computes log_likelihood Function with Kalman Filter Algorithm
 # (Theta are the parameters to optimize and y is the training set)
 def kalman_filter_objfcn(theta, y):
-    phi, sigma_eta = theta  # Solo optimizas dos parámetros
-    a = -1.2704              # Fijo: E[log(ξ²)]
+    phi, sigma_eta = theta  
+    a = -1.2704
     B = 1.0
     H = np.pi**2 / 2
     Q = sigma_eta ** 2
